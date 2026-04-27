0bb3fdef2f6fe78711d46bc2ae5fba7bd8316a7a
GadgetHub123 <tjayyy78@gmail.com>
2026-04-27 15:09:58 +0800

    fix prisma connection

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-import { handlers } from "@/auth";
-
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
 
+import { handlers } from "@/auth";
 export const { GET, POST } = handlers;
