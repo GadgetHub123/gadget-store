1d10ada5fbd44225fad3bebc9560c436e413f9b1
GadgetHub123 <tjayyy78@gmail.com>
2026-04-27 14:38:20 +0800

    fix auth imports

--- auth.ts
+++ auth.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
-import { sendLoginNotificationEmail } from "@/lib/email";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
