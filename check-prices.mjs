976da81d2acd9f5b6886fd26cd947b23e8557588
GadgetHub123 <tjayyy78@gmail.com>
2026-04-28 00:24:29 +0800

    set realistic PHP prices

---
+++ check-prices.mjs
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+  const products = await prisma.product.findMany();
+  products.forEach(p => console.log(p.name + ': ' + p.price));
+  process.exit(0);
+}
+main();
